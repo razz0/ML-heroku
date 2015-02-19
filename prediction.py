138bad398a1ec10ff5487f17af6876a10153c115
Mikko Koho <mikko.koho@csc.fi>
2015-02-19 10:39:20 +0200

    Add scikit-learn

--- prediction.py
+++ prediction.py
@@ -1,11 +1,19 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
+from sklearn.externals import joblib
+from apiharvester import APIHarvester
 
 app = Flask(__name__)
 
+app.debug = True
+
+harvester = APIHarvester()
+prediction_model = joblib.load('model/predictor_model.pkl')
+
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def prediction():
+    forecasts = harvester.fmi_forecast()
+    return render_template('prediction.html', forecasts=forecasts)
+
 
 
