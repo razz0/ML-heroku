c70bcafe0f2c908655fe317a8b5e0d339c870cfb
Mikko Koho <mikko.koho@csc.fi>
2015-02-23 00:49:12 +0200

    Streamlined bootstrap UI

--- prediction.py
+++ prediction.py
@@ -1,17 +1,15 @@
 from collections import defaultdict
-from datetime import datetime, timedelta, time
+from datetime import datetime, timedelta
 import json
 import os
+import urllib2
 
 from flask import Flask, render_template
 import iso8601
 
 from apiharvester import APIHarvester
-from store_predictions import FORECAST_FILE
 from models import prediction_models
 
-
-OBSERVED_DISRUPTIONS_FILE = 'disruptions_observed.json'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -20,68 +18,79 @@ app.debug = True
 if __name__ == '__main__':
     app.run()
 
-apikey = os.environ.get('fmi_apikey')
-harvester = APIHarvester(logfile="harvester.log", apikey=apikey)
+BACKEND_URL = os.environ.get('backend_url')
 
+harvester = APIHarvester(logfile="harvester.log", apikey='dummy')
+
+# TODO: Beaker cache
 
 @app.route('/')
 def prediction():
     disruptions = defaultdict(dict)
-    forecasts = harvester.fmi_forecast()
+    url = '{backend}data/forecasts.json'.format(backend=BACKEND_URL)
+    forecasts = json.loads(urllib2.urlopen(url).read())
+
+    for model in prediction_models:
+        url = '{backend}{file}'.format(backend=BACKEND_URL, file=model.JSON_FILE)
+        app.logger.debug(url)
+        model.stored_disruptions = json.loads(urllib2.urlopen(url).read())
+
+    future_forecasts = {}
 
     for timestamp, values in forecasts.iteritems():
-        for model in prediction_models:
-            value_tuple = (float(values['Precipitation1h']), float(values['Temperature']), float(values['WindSpeedMS']))
-            disruption_amount = model.predict(value_tuple)
-            disruptions[timestamp].update({model.name: disruption_amount})
 
-    return render_template('prediction.html', forecasts=forecasts, disruptions=disruptions)
+        forecast_time = iso8601.parse_date(timestamp).replace(tzinfo=None)
+        now_time = datetime.utcnow()
+        if now_time >= forecast_time:
+            continue
+
+        future_forecasts[timestamp] = values
+
+        for model in prediction_models:
+            disruption_amount = model.stored_disruptions.get(timestamp)
+            disruptions[model.name].update({timestamp: disruption_amount})
+
+    return render_template('prediction.html', forecasts=future_forecasts, disruptions=disruptions)
 
 
 @app.route('/history/')
 def prediction_history():
-    stored_disruptions = defaultdict(dict)
-    stored_observed_disruptions = harvester.read_datafile(OBSERVED_DISRUPTIONS_FILE) or {}
-    stored_forecasts = harvester.read_datafile(FORECAST_FILE) or {}
+    prediction_history = defaultdict(dict)
+    forecast_history = defaultdict(dict)
 
-    observed_disruptions = {}
+    url = '{backend}data/disruptions_observed.json'.format(backend=BACKEND_URL)
+    stored_observed_disruptions = json.loads(urllib2.urlopen(url).read())
+
+    url = '{backend}data/forecasts.json'.format(backend=BACKEND_URL)
+    stored_forecasts = json.loads(urllib2.urlopen(url).read())
 
     for model in prediction_models:
-        model.stored_disruptions = harvester.read_datafile(model.JSON_FILE) or {}
+        url = '{backend}{file}'.format(backend=BACKEND_URL, file=model.JSON_FILE)
+        model.stored_disruptions = json.loads(urllib2.urlopen(url).read())
 
     for timestamp, values in stored_forecasts.iteritems():
+        observation_time = iso8601.parse_date(timestamp).replace(tzinfo=None)
+        now_time = datetime.utcnow()
+        if now_time < observation_time:
+            continue
+
+        forecast_history[timestamp] = values
+
         for model in prediction_models:
             disruption_amount = model.stored_disruptions.get(timestamp, '-')
-            stored_disruptions[timestamp].update({model.name: disruption_amount})
+            prediction_history[model.name].update({timestamp: disruption_amount})
 
-        obs_time = iso8601.parse_date(timestamp).replace(tzinfo=None)
-        now_time = datetime.utcnow()
-        if timedelta(0) < now_time - obs_time < timedelta(days=2):
-            observed_disruptions[timestamp] = harvester.hsl_api(iso8601.parse_date(timestamp))
+        prediction_history[' Actual'].update({timestamp: stored_observed_disruptions.get(timestamp, '-')})
 
-    stored_observed_disruptions.update(observed_disruptions)
-
-    with open(OBSERVED_DISRUPTIONS_FILE, 'w') as f:
-        json.dump(stored_observed_disruptions, f)
-
-    return render_template('history.html', forecasts=stored_forecasts, predicted=stored_disruptions,
-                           actual=stored_observed_disruptions)
+    app.logger.debug(prediction_history)
+    return render_template('prediction.html', forecasts=forecast_history, disruptions=prediction_history)
 
 
 @app.route('/test/')
 def prediction_test():
     forecasts = {}
-    disruption_set = set()
-    for rain in range(-10, 400, 1):
-        for temp in range(-50, -45, 5):
-            for wind in range(50, 60, 10):
-                values = {'Precipitation1h': float(rain) / 10.0, 'Temperature': float(temp) / 10.0, 'WindSpeedMS': float(wind) / 10.0}
-                value_tuple = (values['Precipitation1h'], values['Temperature'], values['WindSpeedMS'])
-                disruptions = prediction_model.predict(value_tuple)[0]
-#                if disruptions not in disruption_set:
-#                    disruption_set.add(disruptions)
-                values.update({'prediction': disruptions})
-                forecasts.update({'%02d_%02d_%s' % (rain, temp, wind): values})
+
+    # TODO: Create form for trying out values
 
     return render_template('prediction.html', forecasts=forecasts)
 
